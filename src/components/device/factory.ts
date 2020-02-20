5642a2e2610087cd427cc1f0cbee00278df29cf7
Christian Asserson <christian@huddly.com>
2020-02-20 13:59:36 +0100

    dwarffish: add support for PID=0x51

--- src/components/device/factory.ts
+++ src/components/device/factory.ts
@@ -11,6 +11,7 @@ export const HUDDLY_BOXFISH_PID = 0x21;
 export const HUDDLY_BOXFISH_PRODTEST_PID = 0x22;
 export const HUDDLY_CLOWNFISH_PID = 0x31;
 export const HUDDLY_DARTFISH_PID = 0x41;
+export const HUDDLY_DWARFFISH_PID = 0x51;
 
 
 export default class DeviceFactory {
@@ -170,6 +171,7 @@ export default class DeviceFactory {
       case HUDDLY_BOXFISH_PID:
       case HUDDLY_BOXFISH_PRODTEST_PID:
       case HUDDLY_DARTFISH_PID:
+      case HUDDLY_DWARFFISH_PID:
         device = new Boxfish(devInstance, transport, uvcControlInterface, logger, cameraDiscoveryEmitter);
         break;
       default:
