f9f6047dae17def6274530bd9deffa4475cd436b
Jon Tore Hafstad <jontore@huddly.com>
2020-05-19 12:55:14 +0200

    Feature) added support for dwarffish 0x51

---
+++ src/components/device/dwarffish.ts
@@ -0,0 +1,63 @@
+import { EventEmitter } from 'events';
+
+import Api from '../api';
+import DefaultLogger from '../../utilitis/logger';
+import ITransport from '../../interfaces/iTransport';
+import Locksmith from '../locksmith';
+import Boxfish from './boxfish';
+import UpgradeOpts from './../../interfaces/IUpgradeOpts';
+import IDetector from '../../interfaces/IDetector';
+import IAutozoomControl from '../../interfaces/IAutozoomControl';
+import InterpolationParams from '../../interfaces/InterpolationParams';
+import ReleaseChannel from './../../interfaces/ReleaseChannelEnum';
+
+export default class Dwarffish extends Boxfish {
+  transport: ITransport;
+  _api: Api;
+  uvcControlInterface: any;
+  logger: DefaultLogger;
+  locksmith: Locksmith;
+  discoveryEmitter: EventEmitter;
+  productName: string = 'Huddly IQ Lite';
+
+  constructor(
+    uvcCameraInstance: any,
+    transport: ITransport,
+    uvcControlInterface: any,
+    logger: DefaultLogger,
+    cameraDiscoveryEmitter: EventEmitter) {
+    super(uvcCameraInstance, transport, uvcControlInterface, logger, cameraDiscoveryEmitter);
+  }
+
+  async ensureAppMode() {
+    throw new Error('ensureAppMode not implemented');
+  }
+
+  getAutozoomControl(): IAutozoomControl {
+    throw new Error('getAutozoomControl not implemented');
+  }
+
+  getDetector(): IDetector {
+    throw new Error('getDetector not implemented');
+  }
+
+  getState(): Promise<any> {
+    throw new Error('getState not implemented');
+  }
+
+  async setInterpolationParams() {
+    throw new Error('setInterpolationParams not implemented');
+  }
+
+  async getInterpolationParams(): Promise<InterpolationParams> {
+    throw new Error('getInterpolationParams not implemented');
+  }
+
+  async upgrade(opts: UpgradeOpts): Promise<any> {
+    throw new Error('upgrade not implemented');
+  }
+
+  async getLatestFirmwareUrl(releaseChannel: ReleaseChannel = ReleaseChannel.STABLE) {
+    throw new Error('getLatestFirmwareUrl not implemented');
+  }
+}
