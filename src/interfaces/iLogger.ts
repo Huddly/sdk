49c325439f2f35ee782abf0e005f6885f282ac0e
Brikend <brikend@huddly.com>
2021-06-03 13:06:16 +0200

    refactor: fix logger interface and implementation

--- src/interfaces/iLogger.ts
+++ src/interfaces/iLogger.ts
@@ -1,9 +1,9 @@
 export default interface ILogger {
-  warn(message: string, component): void;
+  warn(message: string, component: string): void;
 
-  info(message: string, component): void;
+  info(message: string, component: string): void;
 
-  debug(message: string, component): void;
+  debug(message: string, component: string): void;
 
-  error(message: string, stackTrace: string): void;
+  error(message: string, stackTrace: any, component: string): void;
 }
