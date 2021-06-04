c563dec895e06269dacb37a4c0c5abc8d34013e4
Brikend <brikend@huddly.com>
2021-06-04 09:19:38 +0200

    fix: remove the Empty() and Chunk() workarounds
    refactor: Remove unecessary imports

--- src/interfaces/IGrpcTransport.ts
+++ src/interfaces/IGrpcTransport.ts
@@ -1,7 +1,5 @@
 import ITransport from './iTransport';
-import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
 import { HuddlyServiceClient } from '@huddly/huddlyproto/lib/proto/huddly_grpc_pb';
-import * as huddly from '@huddly/huddlyproto/lib/proto/huddly_pb';
 
 /**
  * Interface used to communicate with huddly network devices.
@@ -25,9 +23,4 @@ export default interface IGrpcTransport extends ITransport {
    * @memberof IGrpcTransport
    */
   grpcClient: HuddlyServiceClient;
-
-  // Workaround until the google.protobuf.Empty class is same from sdk and device-api-ip
-  empty: Empty;
-
-  chunk: huddly.Chunk;
 }
