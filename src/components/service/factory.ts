84c2f970e41985f0ab9cbdf3132bdcb177414e0d
Brikend <brikend@huddly.com>
2021-06-14 09:48:17 +0200

    docs: Add jsdocs for the new service classes

--- src/components/service/factory.ts
+++ src/components/service/factory.ts
@@ -4,6 +4,13 @@ import IServiceOpts from '../../interfaces/IServiceOpts';
 import IHuddlyService from './../../interfaces/IHuddlyService';
 
 export default class ServiceFactory {
+  /**
+   * Get a concrete IHuddlyService implementation
+   * @param  {ILogger} logger Logger instance
+   * @param  {IServiceOpts} serviceOpts Service options for initializing and seting up the service communication
+   * @returns An intance of IHuddlyService that can be used to communicate with the corresponding
+   * huddly service running on host.
+   */
   static getService(logger: ILogger, serviceOpts: IServiceOpts): IHuddlyService {
     switch (process.platform) {
       case 'win32':
