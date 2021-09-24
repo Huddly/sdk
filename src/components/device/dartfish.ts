9383fdc006b4b8624443c311abe0a71e39bf4f67
Brikend <brikend@huddly.com>
2021-09-24 13:37:27 +0200

    feat(canvas): Enable/Disable enhance mode
    Add support for enabling or disabling the canvas enhance mode feature through the SDK.

--- src/components/device/dartfish.ts
+++ src/components/device/dartfish.ts
@@ -27,37 +27,67 @@ export default class DartFish extends Boxfish {
   }
 
   async ensureAppMode() {
-    Logger.warn('Attempting to call method [ensureAppMode] on Huddly ONE', 'Huddly Canvas API');
+    Logger.warn('Attempting to call method [ensureAppMode] on Huddly Canvas', 'Huddly Canvas API');
     throw new Error('Method not implemented/supported.');
   }
 
   getFaceBasedExposureControl(): ICnnControl {
-    Logger.warn('Attempting to call method [getFaceBasedExposureControl] on Huddly ONE', 'Huddly Canvas API');
+    Logger.warn('Attempting to call method [getFaceBasedExposureControl] on Huddly Canvas', 'Huddly Canvas API');
     throw new Error('Method not implemented/supported.');
   }
 
   getAutozoomControl(): IAutozoomControl {
-    Logger.warn('Attempting to call method [getAutozoomControl] on Huddly ONE', 'Huddly Canvas API');
+    Logger.warn('Attempting to call method [getAutozoomControl] on Huddly Canvas', 'Huddly Canvas API');
     throw new Error('Method not implemented/supported.');
   }
 
   getDetector(): IDetector {
-    Logger.warn('Attempting to call method [getDetector] on Huddly ONE', 'Huddly Canvas API');
+    Logger.warn('Attempting to call method [getDetector] on Huddly Canvas', 'Huddly Canvas API');
     throw new Error('Method not implemented/supported.');
   }
 
   getState(): Promise<any> {
-    Logger.warn('Attempting to call method [getState] on Huddly ONE', 'Huddly Canvas API');
+    Logger.warn('Attempting to call method [getState] on Huddly Canvas', 'Huddly Canvas API');
     throw new Error('Method not implemented/supported.');
   }
 
   async setInterpolationParams() {
-    Logger.warn('Attempting to call method [setInterpolationParams] on Huddly ONE', 'Huddly Canvas API');
+    Logger.warn('Attempting to call method [setInterpolationParams] on Huddly Canvas', 'Huddly Canvas API');
     throw new Error('Method not implemented/supported.');
   }
 
   async getInterpolationParams(): Promise<InterpolationParams> {
-    Logger.warn('Attempting to call method [getInterpolationParams] on Huddly ONE', 'Huddly Canvas API');
+    Logger.warn('Attempting to call method [getInterpolationParams] on Huddly Canvas', 'Huddly Canvas API');
     throw new Error('Method not implemented/supported.');
+  }
+
+  disableCanvasEnhanceMode() {
+    const payload = {
+      'camera-mode': 'canvas-no-enhance'
+    };
+    return new Promise<void>(async (resolve, reject) => {
+      await this.api.setProductInfo(payload);
+      const newState = await this.api.getProductInfo();
+      if (newState['camera-mode'] == 'canvas-no-enhance') {
+        resolve();
+      } else {
+        reject('Unable to turn off canvas enhance mode!');
+      }
+    });
+  }
+
+  enableCanvasEnhanceMode() {
+    const payload = {
+      'camera-mode': 'canvas'
+    };
+    return new Promise<void>(async (resolve, reject) => {
+      await this.api.setProductInfo(payload);
+      const newState = await this.api.getProductInfo();
+      if (newState['camera-mode'] == 'canvas') {
+        resolve();
+      } else {
+        reject('Unable to turn on canvas enhance mode!');
+      }
+    });
   }
 }
