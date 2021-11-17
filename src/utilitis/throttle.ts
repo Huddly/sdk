6312cc53f3722a8770c65d10ece0b209dcb20a5a
Eskil Høstad <eskil.hostad@huddly.com>
2021-11-17 14:43:22 +0100

    fix: ipDetector now does not emit duplicate or invalid detections

---
+++ src/utilitis/throttle.ts
@@ -0,0 +1,12 @@
+export default function throttle(callback: Function, limit: number): Function {
+  let waiting = false;
+  return function () {
+    if (!waiting) {
+      callback.apply(this, arguments);
+      waiting = true;
+      setTimeout(function () {
+        waiting = false;
+      }, limit);
+    }
+  };
+}
--- tests/components/ipDetector.spec.ts
+++ tests/components/ipDetector.spec.ts
@@ -5,8 +5,10 @@ import IpDetector from '../../src/components/ipDetector';
 import IIPDeviceManager from '../../src/interfaces/iIpDeviceManager';
 import DeviceManagerMock from '../mocks/ipdevicemanager.mock';
 import CameraEvents from '../../src/utilitis/events';
+import Logger from './../../src/utilitis/logger';
 import * as huddly from '@huddly/camera-proto/lib/api/huddly_pb';
 import DetectorOpts, { DetectionConvertion } from './../../src/interfaces/IDetectorOpts';
+import { utils } from 'mocha';
 
 chai.should();
 chai.use(sinonChai);
@@ -31,6 +33,118 @@ describe('IpDetector', () => {
       ipDetector.destroy();
     });
     describe('on detector not initialized', () => {
+      describe('on grpcServer responding with duplicate detections', () => {
+        let getDetectionsStub;
+        const detectionsDummy = new huddly.Detections();
+        detectionsDummy.setTimestamp(100.0);
+        beforeEach(() => {
+          getDetectionsStub = sinon
+            .stub(deviceManager.grpcClient, 'getDetections')
+            .callsArgWith(1, undefined, detectionsDummy);
+        });
+        afterEach(() => {
+          getDetectionsStub.restore();
+        });
+        it('should not emit stale detections', async () => {
+          ipDetector = new IpDetector(deviceManager, {});
+          const cb = sinon.spy();
+          ipDetector.on(CameraEvents.DETECTIONS, cb);
+          await ipDetector.init();
+          clock.tick(ipDetector._UPDATE_INTERVAL);
+          await clock.next();
+          expect(getDetectionsStub.callCount).to.equal(2);
+          expect(cb.callCount).to.equal(1);
+        });
+      });
+
+      describe('on grpcServer responding with invalid detections', () => {
+        let getDetectionsStub;
+        let warnStub;
+        const detectionsDummy = new huddly.Detections();
+        // If timestamp < 0 it means that IMX has not received any detections,
+        // likely due to detector not being started / AZ not on / camera not streaming
+        detectionsDummy.setTimestamp(-1.0);
+        beforeEach(() => {
+          warnStub = sinon.stub(Logger, 'warn');
+          getDetectionsStub = sinon
+            .stub(deviceManager.grpcClient, 'getDetections')
+            .callsArgWith(1, undefined, detectionsDummy);
+        });
+        afterEach(() => {
+          getDetectionsStub.restore();
+          warnStub.restore();
+        });
+        it('should log a warning', async () => {
+          ipDetector = new IpDetector(deviceManager, {});
+          await ipDetector.init();
+          clock.tick(ipDetector._UPDATE_INTERVAL);
+          await clock.next();
+          expect(warnStub).to.have.been.called;
+        });
+        it('should not emit invalid detections', async () => {
+          ipDetector = new IpDetector(deviceManager, {});
+          const cb = sinon.spy();
+          ipDetector.on(CameraEvents.DETECTIONS, cb);
+          await ipDetector.init();
+          clock.tick(ipDetector._UPDATE_INTERVAL);
+          await clock.next();
+          expect(getDetectionsStub.callCount).to.equal(2);
+          expect(cb.callCount).to.equal(0);
+        });
+      });
+
+      describe('on grpcServer responding with distinct detections', () => {
+        let getDetectionsStub;
+        const firstDetectionsDummy = new huddly.Detections();
+        firstDetectionsDummy.setTimestamp(100.0);
+        const secondDetectionsDummy = new huddly.Detections();
+        secondDetectionsDummy.setTimestamp(150.0);
+        beforeEach(() => {
+          getDetectionsStub = sinon
+            .stub(deviceManager.grpcClient, 'getDetections')
+            .onFirstCall()
+            .callsArgWith(1, undefined, firstDetectionsDummy)
+            .onSecondCall()
+            .callsArgWith(1, undefined, secondDetectionsDummy);
+        });
+        afterEach(() => {
+          getDetectionsStub.restore();
+        });
+        it('should emit all distinct detections', async () => {
+          ipDetector = new IpDetector(deviceManager, {});
+          const cb = sinon.spy();
+          ipDetector.on(CameraEvents.DETECTIONS, cb);
+          await ipDetector.init();
+          clock.tick(ipDetector._UPDATE_INTERVAL);
+          await clock.next();
+          expect(getDetectionsStub.callCount).to.equal(2);
+          expect(cb.callCount).to.equal(2);
+        });
+      });
+
+      describe('on querying old L1 versions where timestamp is not set', () => {
+        let getDetectionsStub;
+        const detectionsDummy = new huddly.Detections();
+        beforeEach(() => {
+          getDetectionsStub = sinon
+            .stub(deviceManager.grpcClient, 'getDetections')
+            .callsArgWith(1, undefined, detectionsDummy);
+        });
+        afterEach(() => {
+          getDetectionsStub.restore();
+        });
+        it('should emit all DETECTIONS it receives', async () => {
+          ipDetector = new IpDetector(deviceManager, {});
+          const cb = sinon.spy();
+          ipDetector.on(CameraEvents.DETECTIONS, cb);
+          await ipDetector.init();
+          clock.tick(ipDetector._UPDATE_INTERVAL * 2);
+          await clock.next();
+          expect(getDetectionsStub.callCount).to.equals(3);
+          expect(cb.callCount).to.equals(3);
+        });
+      });
+
       describe('on includeRawDetections is true', () => {
         it('should emit both DETECTIONS and RAWDETECTIONS at initialization', async () => {
           ipDetector = new IpDetector(deviceManager, { includeRawDetections: true });
@@ -43,8 +157,8 @@ describe('IpDetector', () => {
           await clock.next();
           expect(cb.called).to.equal(true);
           expect(rawCb.called).to.equal(true);
-        })
-      })
+        });
+      });
       describe('on includeRawDetections is false', () => {
         it('should only emit DETECTIONS', async () => {
           ipDetector = new IpDetector(deviceManager, { includeRawDetections: false });
@@ -57,19 +171,10 @@ describe('IpDetector', () => {
           await clock.next();
           expect(cb.called).to.equal(true);
           expect(rawCb.called).to.equal(false);
-        })
+        });
       });
 
       describe('on DOWS not set', () => {
-        it('should emit DETECTIONS event at initialization and every update interval', async () => {
-          ipDetector = new IpDetector(deviceManager, {});
-          const cb = sinon.spy();
-          ipDetector.on(CameraEvents.DETECTIONS, cb);
-          await ipDetector.init();
-          clock.tick(ipDetector._UPDATE_INTERVAL * 2);
-          await clock.next();
-          expect(cb.callCount).to.equals(3);
-        });
         it('should call setCnnFeature with correct arg', async () => {
           ipDetector = new IpDetector(deviceManager, {});
           await ipDetector.init();
@@ -78,15 +183,6 @@ describe('IpDetector', () => {
           expect(arg.getMode()).to.equal(huddly.Mode.START);
         });
         describe('On DOWS set', () => {
-          it('should emit DETECTIONS event at initialization and every update interval', async () => {
-            ipDetector = new IpDetector(deviceManager, { DOWS: true });
-            const cb = sinon.spy();
-            ipDetector.on(CameraEvents.DETECTIONS, cb);
-            await ipDetector.init();
-            clock.tick(ipDetector._UPDATE_INTERVAL * 2);
-            await clock.next();
-            expect(cb.callCount).to.equals(3);
-          });
           it('should not call setCnnFeature', async () => {
             ipDetector = new IpDetector(deviceManager, { DOWS: true });
             await ipDetector.init();
@@ -184,6 +280,6 @@ describe('IpDetector', () => {
       ipDetector = new IpDetector(deviceManager, {});
       await ipDetector.updateOpts(newOpts);
       expect(ipDetector._options).to.deep.equals(newOpts);
-    })
-  })
+    });
+  });
 });
