94a292bb38352b32f0dc264bdcb758821e7389b2
Brikend <brikend@huddly.com>
2022-03-31 10:52:26 +0200

    feat: implement #getUpgrader directly on Ace and See controllers

--- tests/components/device/see.spec.ts
+++ tests/components/device/see.spec.ts
@@ -1,9 +1,11 @@
 import chai, { expect } from 'chai';
 import sinonChai from 'sinon-chai';
-import See from './../../../src/components/device/see';
 import IGrpcTransport from '@huddly/sdk-interfaces/lib/interfaces/IGrpcTransport';
 import { EventEmitter } from 'stream';
 import { HuddlyServiceClient } from '@huddly/camera-proto/lib/api/huddly_grpc_pb';
+
+import See from './../../../src/components/device/see';
+import SeeUpgrader from './../../../src/components/upgrader/seeUpgrader';
 
 chai.should();
 chai.use(sinonChai);
@@ -27,5 +29,13 @@ describe('See', () => {
   it('should have correct product name', () => {
     const see = new See({}, new DummyTransport(), new EventEmitter());
     expect(see.productName).to.equal('Huddly S1');
+  });
+
+  describe('#getUpgrader', () => {
+    it('should return an instance of the AceUpgrader', async () => {
+      const see = new See({}, new DummyTransport(), new EventEmitter());
+      const upgrader = await see.getUpgrader();
+      expect(upgrader).to.be.instanceOf(SeeUpgrader);
+    });
   });
 });
