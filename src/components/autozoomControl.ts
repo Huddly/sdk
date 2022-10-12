c8ae97fd631344b040744d09be453ea3b515c264
Christian Asserson <christian@huddly.com>
2022-10-12 15:39:59 +0200

    Add support for focus Genius mode

--- src/components/autozoomControl.ts
+++ src/components/autozoomControl.ts
@@ -319,6 +319,7 @@ export default class AutozoomControl implements IAutozoomControl {
       normal: 0,
       plaza: 1,
       plaza_duplicate: 2,
+      focus: 3,
     };
     Logger.debug(
       `Sending autozoom/set-mode request with param '{ mode: ${modeKeys[mode]} }'`,
