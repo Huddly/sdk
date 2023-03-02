3bff2203434fdaa377d72e26ebbd19ce91191eec
Patrik <patrik@huddly.com>
2023-03-02 11:44:09 +0100

    feat(caClient): added client that communicates with the certificate authority

---
+++ src/utilitis/caClient.ts
@@ -0,0 +1,104 @@
+import Logger from '@huddly/sdk-interfaces/lib/statics/Logger';
+import ICaClient, {
+  OptionCertificate,
+  OptionCertificatesResult,
+  isOptionCertificate,
+} from '@huddly/sdk-interfaces/lib/interfaces/ICaClient';
+import https from 'https';
+
+const PRIMARY_CA_HOST_URL = 'https://public-ca.huddly.com';
+
+// In cases of strict firewall policies we might be able to use an azure domain instead
+const SECONDARY_CA_HOST_URL =
+  'https://hmc-app-public-api-certificateauthority-prod-we.azurewebsites.net';
+
+class CaClient implements ICaClient {
+  /**
+   * Gets any option certificates that exists on the CA server for a given camera
+   *  *
+   * @param serialNumber serial number of the camera to request option certificates for
+   * @returns {Promise<OptionCertificate[]>} A list of option certificates for the given camera
+   */
+  async getOptionCertificates(serialNumber: string): Promise<OptionCertificatesResult> {
+    let errors: string[] = [];
+    for (let hostUrl of [PRIMARY_CA_HOST_URL, SECONDARY_CA_HOST_URL]) {
+      const requestUrl = this._createCaClientRequestUrl(hostUrl, serialNumber);
+      Logger.info(`Requesting option certificates with ${requestUrl}`, CaClient.name);
+      try {
+        const serverReponse = await this._requestOptionCertificatesFromService(requestUrl);
+        const optionCertificatesResult = this._constructValidOptionCertificateResult(serverReponse);
+        return optionCertificatesResult;
+      } catch (error) {
+        Logger.warn(
+          `Encountered error trying to get option certificates from ${hostUrl}: ${
+            error.message || error
+          }`,
+          CaClient.name
+        );
+
+        // We want to store all errors from the different attempts of trying to aquire the option certificates
+        errors.push(error.message || error);
+      }
+    }
+    Logger.warn(errors.toString());
+    const optionCertificateResult: OptionCertificatesResult = {
+      error: errors.toString(),
+    };
+    return optionCertificateResult;
+  }
+
+  _constructValidOptionCertificateResult(serverReponse: any): OptionCertificatesResult {
+    if (!Array.isArray(serverReponse)) {
+      const errorText = `Expected to get a list from option certificate server. Got: ${serverReponse.toString()}`;
+      Logger.warn(errorText, CaClient.name);
+      throw Error(errorText);
+    }
+
+    const optionCertificates = serverReponse.map((serverOptionCert): OptionCertificate => {
+      if (!isOptionCertificate(serverOptionCert)) {
+        const errorText = `Expected option certificate from server to contain attributes 'format', 'option' and 'data', but got ${Object.keys(
+          serverOptionCert
+        ).toString()}`;
+        Logger.warn(errorText, CaClient.name);
+        throw Error(errorText);
+      }
+
+      return {
+        format: serverOptionCert.format,
+        data: serverOptionCert.data,
+        option: serverOptionCert.option,
+      };
+    });
+
+    return {
+      optionCertificates,
+    };
+  }
+
+  _createCaClientRequestUrl(hostUrl: string, serialNumber: string): string {
+    return `${hostUrl}/certificates/options?serialNumber=${serialNumber}`;
+  }
+
+  async _requestOptionCertificatesFromService(requestUrl: string): Promise<Array<any>> {
+    return new Promise((resolve, reject) => {
+      const responseCallack = (response) => {
+        const data: Buffer[] = [];
+
+        response.on('data', (chunk: Buffer) => {
+          data.push(chunk);
+        });
+
+        response.on('end', () => {
+          const parsedData = JSON.parse(Buffer.concat(data).toString());
+          resolve(parsedData);
+        });
+
+        response.on('error', reject);
+      };
+      const request = https.request(requestUrl, responseCallack);
+      request.on('error', reject);
+      request.end();
+    });
+  }
+}
+export default CaClient;
---
+++ tests/utilities/caClient.spec.ts
@@ -0,0 +1,105 @@
+import sinon from 'sinon';
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+import CaClient from './../../src/utilitis/caClient';
+
+chai.should();
+chai.use(sinonChai);
+
+describe.only('CaClient', () => {
+  const optionCert = { format: 'PEM', option: 'speakerframing', data: 'data' };
+  const optionCerts = [optionCert, optionCert];
+  const testError = new Error('There was an issue');
+  let caClient: CaClient;
+  beforeEach(() => {
+    caClient = new CaClient();
+  });
+  describe('createCaClientRequestUrl', () => {
+    it('should return a valid ca request url', () => {
+      const serialNumber = 'serial123';
+      const hostUrl = 'host.com';
+      const expectedRequestUrl = `${hostUrl}/certificates/options?serialNumber=${serialNumber}`;
+      const requestUrl = caClient._createCaClientRequestUrl(hostUrl, serialNumber);
+      expect(expectedRequestUrl).to.equal(requestUrl);
+    });
+  });
+  describe('getOptionCertificates', () => {
+    let _requestOptionCertificatesFromServiceStub;
+    let _constructValidOptionCertificateResult;
+    beforeEach(() => {
+      _requestOptionCertificatesFromServiceStub = sinon.stub(
+        caClient,
+        '_requestOptionCertificatesFromService'
+      );
+    });
+    afterEach(() => {
+      _requestOptionCertificatesFromServiceStub.restore();
+    });
+    it('should return a list of on or more valid option certificates', async () => {
+      _requestOptionCertificatesFromServiceStub.resolves(optionCerts);
+      expect((await caClient.getOptionCertificates('test')).optionCertificates).to.deep.equal(
+        optionCerts
+      );
+    });
+
+    describe('if both hosts has an error', () => {
+      beforeEach(() => {
+        _constructValidOptionCertificateResult = sinon.stub(
+          caClient,
+          '_constructValidOptionCertificateResult'
+        );
+      });
+      afterEach(() => {
+        _constructValidOptionCertificateResult.restore();
+      });
+      it('should return a result with an error when getting the options certificate fails', async () => {
+        _requestOptionCertificatesFromServiceStub.rejects(testError);
+        expect((await caClient.getOptionCertificates('test')).error).to.not.equal(undefined);
+      });
+      it('should return a result with an error when validation fails', async () => {
+        _constructValidOptionCertificateResult.throws(testError);
+        expect((await caClient.getOptionCertificates('test')).error).to.not.equal(undefined);
+      });
+    });
+    describe('if there is an issue with only one host', () => {
+      it('it should return a result with option certificates and without an error', async () => {
+        _requestOptionCertificatesFromServiceStub.onCall(0).rejects(testError);
+        _requestOptionCertificatesFromServiceStub.onCall(1).resolves(optionCerts);
+
+        const result = await caClient.getOptionCertificates('test');
+        expect(result.error).to.equal(undefined);
+        expect(result.optionCertificates).to.deep.equal(optionCerts);
+      });
+    });
+  });
+  describe('_constructValidOptionCertificateResult', () => {
+    describe('top level response is not a list', () => {
+      it('should throw an error', () => {
+        expect(
+          caClient._constructValidOptionCertificateResult.bind(caClient, optionCert)
+        ).to.throw();
+      });
+    });
+    describe('one or more option certificate is not formatted correctly', () => {
+      it('lacks a format attribute', () => {
+        const cert = { formats: 'PEM', option: 'speakerframing', data: 'data' };
+        expect(caClient._constructValidOptionCertificateResult.bind(caClient, [cert])).to.throw();
+      });
+      it('lacks a option attribute', () => {
+        const cert = { formats: 'PEM', options: 'speakerframing', data: 'data' };
+        expect(caClient._constructValidOptionCertificateResult.bind(caClient, [cert])).to.throw();
+      });
+      it('lacks a data attribute', () => {
+        const cert = { formats: 'PEM', option: 'speakerframing', datas: 'data' };
+        expect(caClient._constructValidOptionCertificateResult.bind(caClient, [cert])).to.throw();
+      });
+    });
+    describe('correctly formatted response from the server', () => {
+      it('should return a list with correctly formatted option certificates result', () => {
+        expect(caClient._constructValidOptionCertificateResult(optionCerts)).to.deep.equal({
+          optionCertificates: optionCerts,
+        });
+      });
+    });
+  });
+});
